74644c3b7f607576ed808f226fca301a07958a3e
Jorge Nizama <jorge.nizama@bairesdev.com>
2023-02-13 22:15:49 -0500

    refact name files py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-from my_collections import *
-from functionsmix import *
+from collections import *
+from functions import *
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
